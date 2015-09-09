c40b8aa16753182f3a74d2420a76d9fa0d74b66b
Marc <pachacamac@users.noreply.github.com>
2015-09-09 14:01:53 +0200

    Update waterfall.js

--- waterfall.js
+++ waterfall.js
@@ -41,6 +41,11 @@ var Waterfall = (function(options){
     return (px / width) * 22000;
   };
   
+  var addEventListeners = function(elem, events, f){
+    if(typeof events == 'string'){events = events.split(' ');}
+    for(var i=0; i<events.length; i++){elem.addEventListener(events[i], f)};
+  };
+
   addEventListeners(canvas, "mousemove touchmove", function(e){
     e.preventDefault();
     info.innerText = pxToFreq(e.touches ? e.touches[0].pageX : e.offsetX);
